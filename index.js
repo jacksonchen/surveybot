f90e6e2a8917588d9563d0d29f82e53de7bd0bb3
Jackson Chen <tiger2chen@gmail.com>
2015-11-02 15:42:35 -0700

    Add files

---
+++ index.js
@@ -0,0 +1,32 @@
+var Nightmare = require('nightmare');
+var sem = require('semaphore')(1);
+var counter = 1;
+var n = 10000; // Change iterations here
+
+var yolo = function(sem) {
+  var nightmare = new Nightmare()
+  // .goto("https://docs.google.com/forms/d/1hzPRkrJnkwyF3wlqwwkVLQBWRFbeAZ9_Xch50wj1fbs/viewform")
+  // .click('span.ss-choice-item-control input[name="entry.1066004599"]')
+    .goto("https://docs.google.com/forms/d/1kkBTU_DWRGD2fKkDPhKDxQEjPRIwjjFnmmF_NJf0LV8/viewform")
+    .click('span.ss-choice-item-control input[name="entry.1494608824"]')
+    .click('input[type="submit"]')
+    .run(function(err, nightmare) {
+      if (err) return console.log(err);
+      console.log("Submitted " + counter + " times!");
+      counter = counter + 1;
+      if (counter == (n)) {
+        console.log("exiting")
+        process.exit();
+      }
+      setTimeout(function() {
+        sem.leave();
+      }, 50);
+    })
+    .end();
+}
+
+for (var i = 1; i < n; i++) {
+  sem.take(function() {
+    yolo(sem);
+  });
+}
