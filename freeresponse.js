63c5feb5973cd67d82ae75ee297b57f94eeb373a
Jackson Chen <tiger2chen@gmail.com>
2015-11-13 22:19:24 -0700

    Add free response. Use at your own risk.

---
+++ freeresponse.js
@@ -0,0 +1,69 @@
+var Nightmare = require('nightmare');
+var sem = require('semaphore')(1);
+var philsem = require('semaphore')(1);
+var request = require('request');
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var counter = 1;
+
+var yolo = function(text, callback) {
+  var nightmare = new Nightmare()
+    .goto("https://docs.google.com/forms/d/1dhALobG0RMe7ESPSdpTVnfbccPxF1uQQkmvgsVbjkSI/viewform")
+    .type('textarea[name="entry.300495425"]', text)
+    .click('input[type="submit"]')
+    .run(function(err, nightmare) {
+      if (err) return console.log(err);
+      console.log("Submitted " + counter + " times!");
+      counter = counter + 1;
+      setTimeout(function() {
+        callback();
+      }, 50);
+    })
+    .end();
+}
+
+var philosopher = function(philLink, philName, callback) {
+  request(philLink, function(error, response, html) {
+    $ = cheerio.load(html);
+    var linkArr = $('div.masonryitem div.boxyPaddingBig span.bqQuoteLink a');
+    var philCounter = 0;
+
+    _.forEach(linkArr, function(link) {
+      sem.take(function() {
+        var quote = '\"' + link.children[0].data + '\" - ' + philName;
+        console.log(quote);
+        yolo(quote, function() {
+          sem.leave();
+          if (philCounter >= linkArr.length - 1) {
+            callback();
+          }
+          philCounter = philCounter + 1;
+        });
+      })
+    })
+
+  })
+}
+
+var philosophers = function() {
+  var url = "http://www.brainyquote.com/profession/philosopher_quotes";
+
+  request(url, function(error, response, html) {
+    $ = cheerio.load(html);
+    var linkArr = $('tbody tr td a');
+
+    _.forEach(linkArr, function(link) {
+      philsem.take(function() {
+        var philLink = "http://www.brainyquote.com" + link.attribs.href;
+        var philName = link.children[0].data.replace(/\n/g, '');
+        console.log("========Adding quotes from " + philName + "==========");
+        philosopher(philLink, philName, function() {
+          philsem.leave();
+        });
+      })
+    })
+
+  })
+}
+
+philosophers();
